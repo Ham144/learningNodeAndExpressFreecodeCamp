516c93c381df7cc8955e3ddc8ba2e2b881d6fa72
ham144 <24434muhammad.yafizham@gmail.com>
2024-01-14 16:11:33 +0700

    wedwefrwef

--- fileStream.js
+++ fileStream.js
@@ -4,7 +4,7 @@ import http from 'http'
 http.createServer(
     (req,res)=>{
         const fileStream = fs.createReadStream("./contoh.txt", "utf8")
-        fileStream.on("open", ()=>{
+        fileStream.on("open", ()=>{a
             fileStream.pipe()
         })
         fileStream.on("error", (err) =>{
